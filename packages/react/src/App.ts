3d6adf942d56e66c03f2f3a974e7bc50e1e438d3
Bun Wong <hdwong@gmail.com>
2023-03-10 14:39:44 +0800

    Update 1.0.2-b

--- packages/react/src/App.ts
+++ packages/react/src/App.ts
@@ -1,5 +1,5 @@
 import { createHeadManager, router } from 'b2_inertiajs_core'
-import { createElement, useEffect, useMemo, useState } from 'react'
+import { createElement, useEffect, useMemo, useRef, useState } from 'react'
 import HeadContext from './HeadContext'
 import PageContext from './PageContext'
 
@@ -11,6 +11,9 @@ export default function App({
   titleCallback,
   onHeadUpdate,
 }) {
+  const refPrevComponent = useRef(null);  // 用于保存上次渲染的组件
+  const refChildElement = useRef(null);   // 用于保存最后一次创建的子元素
+
   const [current, setCurrent] = useState({
     component: initialComponent || null,
     page: initialPage,
@@ -30,11 +33,14 @@ export default function App({
       initialPage,
       resolveComponent,
       swapComponent: async ({ component, page, preserveState }) => {
-        setCurrent((current) => ({
-          component,
-          page,
-          key: preserveState ? current.key : Date.now(),
-        }))
+        setCurrent((current) => {
+          refPrevComponent.current = current.component; // 保存上次渲染的组件
+          return {
+            component,
+            page,
+            key: preserveState ? current.key : Date.now(),
+          };
+        })
       },
     })
 
@@ -68,17 +74,28 @@ export default function App({
       return child
     })
 
+  // 生成子元素
+  let childElement;
+  if (! current.component || ! refPrevComponent.current || current.component !== refPrevComponent.current) {
+    // 如果渲染组件发生变化, 则重新生成子组件
+    childElement = renderChildren({
+      Component: current.component,
+      key: current.key,
+      props: current.page.props,
+    });
+    refChildElement.current = childElement;
+  } else {
+    // 否则使用上次的子组件
+    childElement = refChildElement.current;
+  }
+
   return createElement(
     HeadContext.Provider,
     { value: headManager },
     createElement(
       PageContext.Provider,
       { value: current.page },
-      renderChildren({
-        Component: current.component,
-        key: current.key,
-        props: current.page.props,
-      }),
+      childElement,
     ),
   )
 }
